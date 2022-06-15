c2bd07dd020eb2357de54e59f727d91c6d4a6ded
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2022-06-15 17:53:30 +0530

    Added some more details

--- iac/1100100-terraform-console-lists/commands.sh
+++ iac/1100100-terraform-console-lists/commands.sh
@@ -1,4 +1,5 @@
-cd ./iac/11-terraform-console-lists
+
+cd ./iac/1100100-terraform-console-lists
 
 cd ..
 
@@ -30,20 +31,38 @@ element(var.fruits, length(var.fruits)-1)
 [for fruit in var.fruits : fruit]
 [for fruit in var.fruits : upper(fruit)]
 
+var.cloud-providers
+
 # Get the elements as an array
 [for cloud-provider in var.cloud-providers : cloud-provider]
 # The following extracts id and name properties from objects
 [for cloud-provider in var.cloud-providers : cloud-provider.id]
 # and the following the names as an array
 [for cloud-provider in var.cloud-providers : cloud-provider.name]
+# Names in the upper case.
+[for cloud-provider in var.cloud-providers : upper(cloud-provider.name)]
 
 # Splat expression
 # https://www.terraform.io/docs/language/expressions/splat.html
 # Using splat expressions. The following prints out all of the elements as an array.
 var.cloud-providers[*]
+# The above is same as below
+[for cloud-provider in var.cloud-providers : cloud-provider]
+
 # Using splat expressions. The following prints out all the ids as an array.
 var.cloud-providers[*].id
+# The above is same as below
+[for cloud-provider in var.cloud-providers : cloud-provider.id]
+
 # and the following the names as an array
 var.cloud-providers[*].name
+# The above is same as below
+[for cloud-provider in var.cloud-providers : cloud-provider.name]
 
+# and the following the names as an array in upper case
+# The following both are not working. 
+var.cloud-providers[upper(*)].name
+upper(var.cloud-providers[*].name)
+# The following works.
+[for cloud-provider in var.cloud-providers : upper(cloud-provider.name)]
 
