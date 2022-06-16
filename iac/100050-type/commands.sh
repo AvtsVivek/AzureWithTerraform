df2b25c5c2b6488a4bd2ca2678666605fe4c0692
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2022-06-16 16:01:32 +0530

    added a new exericise to demo type

---
+++ iac/100050-type/commands.sh
@@ -0,0 +1,20 @@
+
+# cd into the directory.
+cd ./iac/100050-type
+
+terraform fmt
+
+# Launch Terraform console
+terraform console
+
+type("A simple string") # REturns string type
+
+type(23) # REturns number
+
+type(["Apple", "Ballon"]) # REturns tuple
+
+type([1, 234]) # REturns tuple
+
+type([1, "234sdf"]) # REturns tuple
+
+exit
