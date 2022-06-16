b4b2e44586c3d6ded3e379c474d4b6693d3d5c73
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2022-06-16 16:24:38 +0530

    output wiuth toset

--- iac/900110-toset-var-output/variables.tf
+++ iac/900110-toset-var-output/variables.tf
@@ -2,6 +2,9 @@ variable "fruits" {
   default = ["apple", "banana", "pine apple", "grape", "strawberry"]
 }
 
-output "SomeOutputVar" {
-  value = var.fruits
+output "OutputFruits" {
+  # toset(["straberry", "grape ", "banana", "apple", ])
+  # Not sure how to interpret this.
+  value = toset(var.fruits)
 }
+
