00f52a1ad5cfb2eabf22da7a38ec88a4a196f878
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2022-06-19 16:25:50 +0530

    Added a new folder for tuple my sql threat detection

---
+++ iac/1202500-input-vars-msql-with-threat-dectection-tuple/c4-azure-mysql-database.tf
@@ -0,0 +1,52 @@
+
+# Azure MySQL Database Server
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/mysql_server
+
+resource "azurerm_mysql_server" "viveksqlserver" {
+  name                = "${var.business_unit}-${var.environment}-${var.db_name}" # This needs to be globally unique within Azure.
+  location            = azurerm_resource_group.vivekrg.location
+  resource_group_name = azurerm_resource_group.vivekrg.name
+
+  administrator_login          = var.db_username
+  administrator_login_password = var.db_password
+
+  # sku_name   = "B_Gen5_2"
+  sku_name   = "GP_Gen5_2" # General Purpose Tier
+  storage_mb = var.db_storage_mb
+  version    = "8.0"
+
+  auto_grow_enabled                 = var.db_auto_grow_enabled
+  backup_retention_days             = 7
+  geo_redundant_backup_enabled      = false
+  infrastructure_encryption_enabled = false
+  public_network_access_enabled     = true
+  ssl_enforcement_enabled           = true
+  ssl_minimal_tls_version_enforced  = "TLS1_2"
+  tags                              = var.common_tags
+
+  # threat_detection_policy {
+  #   enabled              = var.tdpolicy.enabled
+  #   retention_days       = var.tdpolicy.retention_days
+  #   email_account_admins = var.tdpolicy.email_account_admins
+  #   email_addresses      = var.tdpolicy.email_addresses
+  # }
+
+  threat_detection_policy {
+    enabled              = var.tdpolicy-tuple[0]
+    retention_days       = var.tdpolicy-tuple[1]
+    email_account_admins = var.tdpolicy-tuple[2]
+    email_addresses      = var.tdpolicy-tuple[3]
+  }
+
+}
+
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/mysql_database
+# Create Database inside Azure MySQL Database Server
+resource "azurerm_mysql_database" "vivekmysqldb1" {
+  name                = "vivekwebappdb1"
+  resource_group_name = azurerm_resource_group.vivekrg.name
+  server_name         = azurerm_mysql_server.viveksqlserver.name
+  charset             = "utf8"
+  collation           = "utf8_unicode_ci"
+}
+
---
+++ iac/1202500-input-vars-msql-with-threat-dectection-tuple/terraform.tfvars
@@ -0,0 +1,22 @@
+
+resource_group_name     = "vivek-mssql-sensitive-rg"
+resource_group_location = "southindia"
+
+# DB Variables
+db_name              = "vivek-mysql"
+db_storage_mb        = 5120
+db_auto_grow_enabled = true
+
+
+
+
+# Generic Variables
+business_unit = "it"
+environment   = "dev"
+
+
+
+# DB Variables
+
+
+tdpolicy-tuple = [true, 10, true, ["dkalyanreddy@gmail.com", "stacksimplify@gmail.com"]]
