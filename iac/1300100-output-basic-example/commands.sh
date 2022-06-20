67b48caf966c46fc7e714deee8b59038b7901cbf
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2022-06-20 17:49:21 +0530

    Added new folder for demoing output for vnet example

---
+++ iac/1300100-output-basic-example/commands.sh
@@ -0,0 +1,24 @@
+
+cd iac\1300100-output-basic-example
+
+cd ..\..
+
+code . -r
+
+terraform init
+
+terraform fmt # formats the tf files.
+
+terraform validate
+
+terraform plan -out main.tfplan
+
+terraform show main.tfplan
+
+terraform apply main.tfplan
+
+terraform plan -destroy -out main.destroy.tfplan
+
+terraform show main.destroy.tfplan
+
+terraform apply main.destroy.tfplan
