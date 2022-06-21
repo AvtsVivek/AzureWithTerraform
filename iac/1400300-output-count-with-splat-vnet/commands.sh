ca632e09b8c675d480a8cec65f32f16ade3dcb53
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2022-06-21 11:04:20 +0530

    Added new folder for demoing output for vnet example

---
+++ iac/1400300-output-count-with-splat-vnet/commands.sh
@@ -0,0 +1,39 @@
+
+cd iac\1400300-output-count-with-splat-vnet
+
+cd ..\..
+
+code . -r
+
+terraform init
+
+terraform fmt # formats the tf files.
+
+terraform validate
+
+terraform plan -out main.tfplan
+
+terraform show main.tfplan
+
+terraform apply main.tfplan
+
+terraform state list
+
+terraform output
+
+terraform output resource_group_id
+
+# Note the following and the output will be shown. Its sensitive and even then it will be shown.
+terraform output virtual_network_names
+
+# Even if these are sensitive, they will not be shown.
+terraform output virtual_network_ids
+
+# Even if these are sensitive, they will not be shown.
+terraform output -json
+
+terraform plan -destroy -out main.destroy.tfplan
+
+terraform show main.destroy.tfplan
+
+terraform apply main.destroy.tfplan
