f30f3e34edea1d91ea91e231b2968143a2001625
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2022-06-23 14:17:10 +0530

    Added new folder

---
+++ iac/1800100-Terraform-Remote-State-Storage-and-Locking/tf1.providers.tf
@@ -0,0 +1,24 @@
+# Terraform Block
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.0"
+    }
+  }
+
+  # Terraform State Storage to Azure Storage Container
+  backend "azurerm" {
+    resource_group_name  = "storage-rg"
+    storage_account_name = "tfstatetrial"
+    container_name       = "tfstatefiles"
+    key                  = "terraform.tfstate"
+  }
+
+}
+
+# Provider Block
+provider "azurerm" {
+  features {}
+}
