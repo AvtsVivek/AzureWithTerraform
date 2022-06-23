b6ca7a8f7e7670229f1e96cebf08bbdf4fd37c11
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2022-06-23 18:00:41 +0530

    Added new folder remote state datasource

--- iac/1800400-remote-state-datasource/tf3.output.tf
+++ iac/1800400-remote-state-datasource/tf3.output.tf
@@ -1,13 +1,3 @@
-# Terraform Remote State Datasource
-data "terraform_remote_state" "project1" {
-  backend = "azurerm"
-  config = {
-    resource_group_name  = "storage-rg"
-    storage_account_name = "tfstatetrial"
-    container_name       = "tfstatefiles"
-    key                  = "terraform.tfstate"
-  }
-}
 
 output "remote_state_from_datasource" {
   value = data.terraform_remote_state.project1
