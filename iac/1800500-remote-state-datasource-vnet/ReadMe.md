4a71ecb781a2c2413520b3a279f6f3350a7016c5
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2022-06-23 19:47:47 +0530

    Completed remote storage

---
+++ iac/1800500-remote-state-datasource-vnet/ReadMe.md
@@ -0,0 +1,6 @@
+This creates a virtual network. But this virtual network is create in a resource group.
+
+The resource group name and location is not available directly here in this config files.
+
+Thiese details are present in the remote state file. And this is fetched from the data block.
+
---
+++ iac/1800600-remote-state-datasource-vm-full-example/project-1-network/tf1.providers.tf
@@ -0,0 +1,38 @@
+# Terraform Block
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 2.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.0"
+    }
+  }
+  # Terraform State Storage to Azure Storage Container
+  backend "azurerm" {
+
+    resource_group_name  = "storage-rg"
+    storage_account_name = "tfstatetrial"
+    container_name       = "tfstatefiles"
+    key                  = "net.terraform.tfstate"
+
+  }
+}
+
+# Provider Block
+provider "azurerm" {
+  features {}
+}
+
+# Random String Resource
+resource "random_string" "myrandom" {
+  length  = 6
+  upper   = false
+  special = false
+  numeric = false
+}
+
+
