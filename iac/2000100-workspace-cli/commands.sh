3277cf0a070e8d6e28503ce36b5798b92ef88018
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2022-06-27 18:11:26 +0530

    Added new folder workspace

---
+++ iac/2000100-workspace-cli/commands.sh
@@ -0,0 +1,32 @@
+
+cd iac\2000100-workspace-cli
+
+cd ..\..
+
+# Terraform follows, the following workflow.
+
+# Init, Plan, Validate, Apply, and then Destroy
+
+terraform init
+
+terraform fmt # formats the tf files.
+
+terraform validate
+
+terraform plan -out main.tfplan
+
+terraform show main.tfplan
+
+terraform apply main.tfplan
+
+terraform state list
+
+# For the following command to work, you need to pass on the resource, or data source.
+# This resource or data source is got from terraform state list command
+terraform state show 
+
+terraform plan -destroy -out main.destroy.tfplan
+
+terraform show main.destroy.tfplan
+
+terraform apply main.destroy.tfplan
---
+++ iac/2000100-workspace-cli/main.tf
@@ -0,0 +1,4 @@
+
+output "DefaultWorkspaceName" {
+  value = terraform.workspace
+}
