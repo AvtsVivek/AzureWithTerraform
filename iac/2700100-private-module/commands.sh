eb43c4d9c37c43a96fd916d3f74f15e0b1b75578
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2022-07-09 13:16:05 +0530

    Added new exercise

---
+++ iac/2700100-private-module/commands.sh
@@ -0,0 +1,51 @@
+
+cd iac\2700100-private-module
+
+cd ..\..
+
+terraform fmt
+
+terraform init
+
+# Login and get the token
+terraform login
+
+terraform init
+
+# Now verify the .terraform directory. Inside it, there should be a modules folder. And then inside of it azure_static_website.
+
+terraform validate
+
+terraform plan -out main.tfplan
+
+terraform apply main.tfplan
+
+terraform plan -destroy -out main.destroy.tfplan
+
+terraform show main.destroy.tfplan
+
+terraform apply main.destroy.tfplan
+
+# Now understanding the get command.
+
+# First delete the modules folder
+
+rm -r -fo .terraform/modules
+
+Remove-Item -Recurse -Force .terraform/modules
+
+Remove-Item -Recurse -Force .terraform
+
+Remove-Item terraform.tfstate
+
+Remove-Item terraform.tfstate.backup
+
+Remove-Item main.tfplan
+
+Remove-Item main.destroy.tfplan
+
+Remove-Item .terraform.lock.hcl
+
+
+# terraform get downloads only the modules that are not already downloaded.
+terraform get
---
+++ iac/2700100-private-module/tf3-static-website.tf
@@ -0,0 +1,27 @@
+# Call our Custom Terraform Module which we built earlier
+
+module "azure_static_website" {
+
+  # https://docs.github.com/en/authentication/connecting-to-github-with-ssh
+  # You can use, this or the following.
+  # source = "github.com/AvtsVivek/terraform-azurerm-staticwebstitepublic"
+  # source = "git@github.com:AvtsVivek/terraform-azurerm-staticwebstitepublic.git"
+  # source = "git::https://github.com/AvtsVivek/terraform-azurerm-staticwebstitepublic.git"
+  # source = "git::https://github.com/AvtsVivek/terraform-azurerm-staticwebstitepublic.git?ref=2.0.0"
+  source = "app.terraform.io/vivek-hcta-azure-demo1/staticwebsiteprivate/azurerm"
+
+  version = "2.0.0"
+  # Resource Group
+  location            = "eastus"
+  resource_group_name = "myrg1"
+
+  # Storage Account
+  storage_account_name              = "staticwebsite"
+  storage_account_tier              = "Standard"
+  storage_account_replication_type  = "LRS"
+  storage_account_kind              = "StorageV2"
+  static_website_index_document     = "index.html"
+  static_website_error_404_document = "error.html"
+  static_files_list                 = ["error.html", "index.html"]
+}
+
