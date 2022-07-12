a7e9a101b515cc2e45ac7d92391245d0d08401d8
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2022-07-12 18:31:37 +0530

    Renamed the folder

--- iac/3401100-external-provider-bash-simple-exmpl/main.tf
+++ iac/3401100-external-provider-bash-simple-exmpl/main.tf
@@ -3,12 +3,8 @@ provider "azurerm" {
   features {}
 }
 
-data "external" "simple-jq" {
-  #program = ["bash", ".${path.module}/simple.sh"]
+data "external" "simple-jq-test" {
+
   program = ["bash", "./simple.sh"]
 
-  query = {
-    key_name        = "terraformdemo"
-    key_environment = "dev"
-  }
 }
