b101b07b9c4c110c2aeb4dfac3c953b3ed9e0366
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2022-07-12 11:07:50 +0530

    Refactored locals to a new file

--- iac/3101200-nsg-with-dynamic-block-map/tf3-network-security-group-dynamic-block.tf
+++ iac/3101200-nsg-with-dynamic-block-map/tf3-network-security-group-dynamic-block.tf
@@ -1,39 +1,4 @@
 # Define Ports as a list in locals block
-locals {
-  ports = [22, 80, 8080, 8081, 7080, 7081]
-}
-
-locals {
-  rules_ports_with_description = [
-    {
-      port        = 22
-      description = "Ingress rules for port 22"
-    },
-    {
-      port        = 80
-      description = "Ingress rules for port 80"
-    },
-    {
-      port        = 8080
-      description = "Ingress rules for port 8080"
-    },
-    {
-      port        = 8081
-      description = "Ingress rules for port 8081"
-    },
-    {
-      port        = 7080
-      description = "Ingress rules for port 7080"
-    },
-    {
-      port        = 7081
-      description = "Ingress rules for port 7081"
-    },
-    {
-      port        = 443
-      description = "Ingress rules for port 443"
-  }]
-}
 
 # Resource-2: Create Network Security Group
 resource "azurerm_network_security_group" "mynsg2" {
