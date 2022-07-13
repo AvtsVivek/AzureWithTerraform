3269ddc7d1a0e4abae27120f5015c406719dc69e
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2022-07-13 12:51:47 +0530

    Changed the folder structure

--- iac/3401200-external-provider-bash-ssh-keygen/command.sh
+++ iac/3401200-external-provider-bash-ssh-keygen/command.sh
@@ -1,9 +1,9 @@
 
 # Run the following command ni git bash command prompt. 
 # Running this on powershell prompt will not work.
-cd iac/3401200-external-provider-bash-exmpl/
+cd iac/3401200-external-provider-bash-ssh-keygen/
 
-cd ..\..
+cd ../..
 
 cd ..
 
@@ -19,7 +19,6 @@ terraform apply -auto-approve
 
 terraform plan --destroy
 
-# The following is not working.
+# Ensure you are in bash prompt.
+cd iac/3401200-external-provider-bash-exmpl/
 echo '{"key_name": "terraformdemo", "key_environment": "dev"}' | ./script-dir/ssh-key-gen.sh
-# But the following is working.
-echo '{"key_name": "terraformdemo", "key_environment": "dev"}' | ./script-dir/ssh-key-generator.sh
