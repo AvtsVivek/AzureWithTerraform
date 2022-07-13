88fdaa305675ed07f6102a718cf0011b32f492ea
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2022-07-13 17:57:26 +0530

    Provider managment

--- iac/3601000-provider-management/ReadMe.md
+++ iac/3601000-provider-management/ReadMe.md
@@ -1,3 +1,7 @@
 # Provider Management.
 
 - Dscusses provider managemetn
+
+- (Provider Mirror](https://www.terraform.io/cli/commands/providers/mirror)
+- # The terraform providers mirror command downloads the providers required for the current configuration and copies them into a directory in the local filesystem.
+- # This part of explicit installation method configuration, when you can specify from where the get the provider bits
--- iac/3601000-provider-management/terraform-manifests/commands.sh
+++ iac/3601000-provider-management/terraform-manifests/commands.sh
@@ -30,6 +30,27 @@ terraform providers lock
 # This must be done before init for enterprise senarios.
 terraform providers lock -platform=windows_amd64 -platform=darwin_amd64 -platform=linux_amd64
 
+# The terraform providers mirror command downloads the providers required for the current configuration and copies them into a directory in the local filesystem.
+# This part of explicit installation method configuration, when you can specify from where the get the provider bits
+
+New-Item -Path '.\MirrorFolder' -ItemType Directory
+
+terraform provider mirror 
+
+terraform providers mirror ./MirrorFolder
+
+rm -r -fo ./MirrorFolder
+
+# Schema 
+
+# The `terraform providers schema` command is used to **print detailed schemas** for the providers used in the current configuration.
+
+# Ensure to Terraform Initialize (Ensure terraform working directory is initialized before schema command)
+terraform init
+
+# Terraform Providers Schema
+terraform providers schema -json | jq
+
 rm -r -fo .terraform/modules
 
 Remove-Item -Recurse -Force .terraform/modules
