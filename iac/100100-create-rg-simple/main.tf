07f6453dbbdb7f206a233c33da8c3137002a4127
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2022-07-14 10:42:25 +0530

    Corrected the version

--- iac/100100-create-rg-simple/main.tf
+++ iac/100100-create-rg-simple/main.tf
@@ -1,27 +1,18 @@
 # Terraform settings Block
 terraform {
-  required_version = ">=1.0.0"
+  required_version = "~> 1.0"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm" # https://registry.terraform.io/providers/hashicorp/azurerm/latest
-      version = "~>3.0"
-      # version = "2.85.0"
+      version = "~> 3.0"
     }
   }
 }
 
 provider "azurerm" {
-  features {
-  }
-
-  #  subscription_id   = ${env.ARM_SUBSCRIPTION_ID}
-  #  tenant_id         = ${env.ARM_TENANT_ID}
-  #  client_id         = ${env.ARM_CLIENT_ID}
-  #  client_secret     = ${env.ARM_CLIENT_SECRET}
-
+  features { }
 }
 
-# Your code goes here
 # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/resource_group
 resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
