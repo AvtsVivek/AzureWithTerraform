8fa0beb93b485d4a05891f521cbbf404fee164ff
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2022-07-14 18:42:46 +0530

    Removed comments

--- iac/2100100-null-resource-vars-output/main.tf
+++ iac/2100100-null-resource-vars-output/main.tf
@@ -27,10 +27,6 @@ variable "some_var" {
 
 resource "null_resource" "null_resource_simple" {
   for_each = var.some_var
-  # provisioner "local-exec" {
-  #   command = "echo ${each.key} ${each.value.name1} ${each.value.name2}"
-  #   working_dir = "local-exec-output-files/"
-  # }
 }
 
 output "SomeOutputVar" {
--- iac/2100200-null-resource-locals-output/main.tf
+++ iac/2100200-null-resource-locals-output/main.tf
@@ -17,10 +17,6 @@ locals {
 
 resource "null_resource" "null_resource_simple" {
   for_each = local.map1
-  # provisioner "local-exec" {
-  #   command = "echo ${each.key} ${each.value.name1} ${each.value.name2}"
-  #   working_dir = "local-exec-output-files/"
-  # }
 }
 
 output "SomeOutputVar" {
