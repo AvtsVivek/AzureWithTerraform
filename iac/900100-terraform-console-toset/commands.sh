f94eb8f6df6ecf27f79517ba821a1d91313a8ce1
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2022-07-17 08:07:43 +0530

    Inhanced toset exercise

--- iac/900100-terraform-console-toset/commands.sh
+++ iac/900100-terraform-console-toset/commands.sh
@@ -6,25 +6,35 @@ terraform fmt
 # Launch Terraform console
 terraform console
 
+type(["straberry", "grape ", "banana", "apple", ])
+
 # All Strings to Strings. In a set order does not matter.
 toset(["straberry", "grape ", "banana", "apple", ])
 
 type(toset(["straberry", "grape ", "banana", "apple", ]))
+
+type(["Mumbai", "Delhi", 123, 456])
 
 # Mixed Type (Strings and Numbers) - Converted to Strings 
 toset(["Mumbai", "Delhi", 123, 456])
 
 type(toset(["Mumbai", "Delhi", 123, 456]))
 
+type(["z", "k", "r", "a", "k"])
+
 # Removes duplicates (Set collections are unordered and cannot contain duplicate values,) 
 toset(["z", "k", "r", "a", "k"])
 
 type(toset(["z", "k", "r", "a", "k"]))
 
+type([4, 100, 20, 11, 21, 7, 6, 4, 100])
+
 # Also arranges in the order (The order provided will be gone) - In short set collections are unordered
 toset([4, 100, 20, 11, 21, 7, 6, 4, 100])
 
 type(toset([4, 100, 20, 11, 21, 7, 6, 4, 100]))
+
+type(var.fruits)
 
 # Using Variables
 toset(var.fruits)
