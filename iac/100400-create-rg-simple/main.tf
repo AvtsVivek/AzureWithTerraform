2d8fea872be764debba03fe5594f13f6ae6a2a16
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2022-07-19 15:03:11 +0530

    Added more infor

--- iac/100400-create-rg-simple/main.tf
+++ iac/100400-create-rg-simple/main.tf
@@ -10,8 +10,24 @@ terraform {
 }
 
 provider "azurerm" {
-  features { }
+  features {}
 }
+
+# If you uncomemnt the following line, you will get a error about the following resource:
+
+provider "azurerm" {
+  features {}
+}
+
+#╷
+#│ Error: Duplicate provider configuration
+#│
+#│   on main.tf line 16:
+#│   16: provider "azurerm" {
+#│
+#│ A default (non-aliased) provider configuration for "azurerm" was already given at main.tf:12,1-19. If multiple configurations are required, set the "alias" argument for
+#│ alternative configurations.
+#╵
 
 # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/resource_group
 resource "azurerm_resource_group" "rg" {
